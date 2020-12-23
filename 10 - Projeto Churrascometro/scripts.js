789f5152b233718edb7754496e331bf079387eac
danteaquiles <evertonnatandl@outlook.com>
2020-12-23 16:46:57 -0300

    Finalizando o Projeto Churrascômetro

--- 10 - Projeto Churrascometro/scripts.js
+++ 10 - Projeto Churrascometro/scripts.js
@@ -14,7 +14,12 @@ function calcular() {
     let duracao = inputDuracao.value
 
     let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas)
-    console.log(qtdTotalCarne)
+    let qtdTotalCerveja = cervejaPP(duracao) * adultos 
+    let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas)
+
+   resultado.innerHTML = `<p>${qtdTotalCarne / 1000} Kgs de carne</p>`
+   resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja / 355) } Latas de cerveja</p>`
+   resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas / 2000) } Garrafas de refrigerante</p>`
 }
 
 function carnePP(duracao) {
@@ -22,5 +27,21 @@ function carnePP(duracao) {
         return 650
     } else {
         return 400
+    }
+}
+
+function cervejaPP(duracao) {
+    if (duracao >= 6) {
+        return 2000
+    } else {
+        return 1200
+    }
+}
+
+function bebidasPP(duracao) {
+    if (duracao >= 6) {
+        return 1500
+    } else {
+        return 1000
     }
 }
