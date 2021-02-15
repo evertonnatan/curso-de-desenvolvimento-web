04b025af2a96f4872b5b99ea9aeeb95f35d25e7e
evertonnatan <evertonnatandl@outlook.com>
2021-02-15 09:41:10 -0300

    Flipando as cartas

--- 16 - projeto jogo da memoria/assets/scripts/script.js
+++ 16 - projeto jogo da memoria/assets/scripts/script.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front"
 const BACK = "card_back"
+const CARD = "card"
+const ICON = "icon"
 
 let members = [
     'itachi',
@@ -21,12 +23,40 @@ startGame()
 function startGame() {
     cards = createCardsFromMembers(members)
     shuffleCards(cards)
-
     initializeCards(cards)
 }
 
 function initializeCards(cards) {
     let gameBoard = document.getElementById("gameBoard")
+
+    cards.forEach(card => {
+        let cardElement = document.createElement('div')
+        cardElement.id = card.id
+        cardElement.classList.add(CARD)
+        cardElement.dataset.icon = card.icon
+        createCardContent(card, cardElement)
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement)
+    })
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace( FRONT, card, cardElement)
+    createCardFace( BACK, card, cardElement)
+}
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement('div')
+    cardElementFace.classList.add(face)
+    if (face === FRONT) {
+        let iconElement = document.createElement('img')
+        iconElement.classList.add(ICON)
+        iconElement.src = "./assets/images/" + card.icon + ".png"
+        cardElementFace.appendChild(iconElement)
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt"
+    }
+    element.appendChild(cardElementFace)
 }
 
 function shuffleCards(cards) {
@@ -46,9 +76,9 @@ function shuffleCards(cards) {
 function createCardsFromMembers(members) {
     let cards = []
 
-    for(let member of members) {
+    members.forEach(member => {
         cards.push(createPairFromMember(member))
-    }
+    })
 
     return cards.flatMap(pair => pair)
 }
@@ -68,4 +98,8 @@ function createPairFromMember(member) {
 
 function createIdWithMember(member) {
     return member + parseInt(Math.random() * 1000)
+}
+
+function flipCard() {
+    this.classList.add("flip")
 }
