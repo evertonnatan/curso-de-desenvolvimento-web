cbec15b87c7e1fdb3415f94dbd0b379064f99dc4
evertonnatan <evertonnatandl@outlook.com>
2021-03-06 20:54:42 -0300

    Continuando o estudo de Jquery

--- 18 - Jquery/script.js
+++ 18 - Jquery/script.js
@@ -4,3 +4,18 @@ let jq = $("#lista").slideUp(2000)
                     .slideDown(2000)
                     .fadeOut(1500)
 console.log(jq)
+
+// O Jquery é um conjunto muito grande de funções
+
+let objeto = {
+    print10: function () {
+        console.log(10)
+        return this
+    },
+    print20: function () {
+        console.log(20)
+        return this
+    }
+}
+
+objeto.print10().print10().print20().print20().print10().print10()
