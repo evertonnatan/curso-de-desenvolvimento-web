84a02338b77871d2a7dfbd78df66d76e2df4657a
evertonnatan <evertonnatandl@outlook.com>
2021-03-06 20:49:13 -0300

    Continuando o estudo de Jquery

---
+++ 18 - Jquery/script.js
@@ -0,0 +1,6 @@
+// A cada execução da função, o Jquery retorna o própŕio objeto.
+
+let jq = $("#lista").slideUp(2000)
+                    .slideDown(2000)
+                    .fadeOut(1500)
+console.log(jq)
