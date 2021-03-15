17ab400e6114904ea02beae2cba0558d5555f0f1
evertonnatan <evertonnatandl@outlook.com>
2021-03-15 09:29:31 -0300

    Trabalhando com arquivos estaticos com express

--- 22 - express/index.js
+++ 22 - express/index.js
@@ -1,7 +1,10 @@
 // IMPORTANDO O EXPRESS:
 const express = require('express')
+const path = require('path')
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'client')))
 
 const PORT = 5000
 app.listen(PORT, () => {
