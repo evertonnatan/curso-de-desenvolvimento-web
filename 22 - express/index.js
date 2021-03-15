3a415e4691c8def617a56e0395e75096736d907f
evertonnatan <evertonnatandl@outlook.com>
2021-03-15 09:46:03 -0300

    Metodo GET com express

--- 22 - express/index.js
+++ 22 - express/index.js
@@ -4,7 +4,23 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'client')))
+app.use("/meusite", express.static(path.join(__dirname, 'client')))
+
+app.get("/", (req, res) => {
+    // res.set("Content-Type", "text/html")
+
+    res.type("html")
+    res.send("<h1>Hello World From GET</h1>")
+
+    //TIPOS MAIS COMUNS:
+
+    // res.type('.html') => 'text/html'
+    // res.type('html') => 'text/html'
+    // res.type('json') => 'application/json'
+    // res.type('application/json') => 'application/json'
+    // res.type('png') => 'image/png'
+    // res.type('txt') => 'text/plain'
+})
 
 const PORT = 5000
 app.listen(PORT, () => {
