7d6c9ded3ea12b29b081da594826cb49c8a90921
evertonnatan <evertonnatandl@outlook.com>
2021-03-16 09:21:12 -0300

    Instalando e testando requisiçoes com o Insomnia

--- 22 - express/index.js
+++ 22 - express/index.js
@@ -6,13 +6,34 @@ const app = express()
 
 app.use("/meusite", express.static(path.join(__dirname, 'client')))
 
-app.get("/", (req, res) => {
-    // res.set("Content-Type", "text/html")
+app.get("/", (req, res) => {    
+    res.send("<h1>Hello World From GET</h1>")    
+})
 
-    res.type("html")
-    res.send("<h1>Hello World From GET</h1>")
+app.post("/", (req, res) => {
+    res.send("<h1>Hello World From POST</h1>")
+})
 
-    //TIPOS MAIS COMUNS:
+app.put("/", (req, res) => {
+    res.send("<h1>Hello World From PUT</h1>")
+})
+
+app.delete("/", (req, res) => {
+    res.send("<h1>Hello World From DELETE</h1>")
+})
+
+const PORT = 5000
+app.listen(PORT, () => {
+    console.log(`Server Running on Port:${PORT}`)
+})
+
+
+
+
+
+
+
+//TIPOS MAIS COMUNS:
 
     // res.type('.html') => 'text/html'
     // res.type('html') => 'text/html'
@@ -20,9 +41,3 @@ app.get("/", (req, res) => {
     // res.type('application/json') => 'application/json'
     // res.type('png') => 'image/png'
     // res.type('txt') => 'text/plain'
-})
-
-const PORT = 5000
-app.listen(PORT, () => {
-    console.log(`Server Running on Port:${PORT}`)
-})
