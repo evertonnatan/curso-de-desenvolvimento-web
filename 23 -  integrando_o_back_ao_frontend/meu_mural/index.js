c807a91aabf35e6a9a8df63b0f62f79df7d25b26
evertonnatan <evertonnatandl@outlook.com>
2021-03-18 08:48:31 -0300

    Gerando Id's randomicos

--- 23 -  integrando_o_back_ao_frontend/meu_mural/index.js
+++ 23 -  integrando_o_back_ao_frontend/meu_mural/index.js
@@ -10,24 +10,26 @@ let posts = [
         title: "Teste do Mural",
         description: "Descrição teste"
     },
-    {
-        id: "jfkajsdfua",
-        title: "Teste do Mural",
-        description: "Descrição teste"
-    },
-    {
-        id: "jfkajsdfua",
-        title: "Teste do Mural",
-        description: "Descrição teste"
-    },
 ]
 
 app.get("/all", (req, res) => {
     res.json(JSON.stringify(posts))
 })
 
-app.post("/new", (req, res) => {})
+app.post("/new", bodyParser.json(), (req, res) => {
+    let id = generateID()
+    let title = req.body.title
+    let description = req.body.description
+
+    posts.push({ id, title, description })
+
+    res.send("Post Adicionado")
+})
 
 app.listen(PORT, () => {
     console.log("Server running on port", PORT)
 })
+
+function generateID() {
+    return Math.random().toString(36).substring(2, 9)
+}
