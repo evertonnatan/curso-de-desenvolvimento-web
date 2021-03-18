7295975f380cff51c0fcd55a57c51a3b812c5423
evertonnatan <evertonnatandl@outlook.com>
2021-03-18 09:06:18 -0300

    Separando o projeto em arquivos

--- 23 -  integrando_o_back_ao_frontend/meu_mural/index.js
+++ 23 -  integrando_o_back_ao_frontend/meu_mural/index.js
@@ -1,27 +1,20 @@
 const PORT = 3000
 const express = require('express')
 const bodyParser = require('body-parser')
+const posts = require('./model/posts')
 
 const app = express()
 
-let posts = [
-    {
-        id: "jfkajsdfua",
-        title: "Teste do Mural",
-        description: "Descrição teste"
-    },
-]
-
 app.get("/all", (req, res) => {
-    res.json(JSON.stringify(posts))
+    res.json(JSON.stringify(posts.getAll()))
 })
 
 app.post("/new", bodyParser.json(), (req, res) => {
-    let id = generateID()
+    
     let title = req.body.title
     let description = req.body.description
 
-    posts.push({ id, title, description })
+    posts.newPost(title, description)
 
     res.send("Post Adicionado")
 })
@@ -30,6 +23,3 @@ app.listen(PORT, () => {
     console.log("Server running on port", PORT)
 })
 
-function generateID() {
-    return Math.random().toString(36).substring(2, 9)
-}
