3369aeb09f3d8e051c1edf0ca450c547577d15a9
evertonnatan <evertonnatandl@outlook.com>
2021-03-18 09:22:28 -0300

    Separando o projeto em rotas

--- 23 -  integrando_o_back_ao_frontend/meu_mural/index.js
+++ 23 -  integrando_o_back_ao_frontend/meu_mural/index.js
@@ -1,23 +1,12 @@
 const PORT = 3000
 const express = require('express')
-const bodyParser = require('body-parser')
-const posts = require('./model/posts')
+const apiRoute = require('./routes/api')
+const path = require('path')
 
 const app = express()
 
-app.get("/all", (req, res) => {
-    res.json(JSON.stringify(posts.getAll()))
-})
-
-app.post("/new", bodyParser.json(), (req, res) => {
-    
-    let title = req.body.title
-    let description = req.body.description
-
-    posts.newPost(title, description)
-
-    res.send("Post Adicionado")
-})
+app.use(express.static(path.join(__dirname, "public")))
+app.use('/api', apiRoute)
 
 app.listen(PORT, () => {
     console.log("Server running on port", PORT)
