d9b9baf3c4880045bb1908990678af589fffb047
evertonnatan <evertonnatandl@outlook.com>
2021-03-21 09:04:58 -0300

    Usando 'for' no EJS

--- 24 - template_engine-EJS/templatecode/index.js
+++ 24 - template_engine-EJS/templatecode/index.js
@@ -3,16 +3,27 @@ const fs = require('fs')
 const path = require('path')
 const app = express()
 
-let user = {
-    name:"Everton Nathan", 
-    phone: "(566)7896-6541"
-}
+let users = [
+    {
+        name:"Everton Nathan", 
+        phone: "(566)7896-8541"
+    },
+    {
+        name:"Everton Madara", 
+        phone: "(566)7896-6541"
+    },
+    {
+        name:"Everton Dante", 
+        phone: "(566)7896-4141"
+    },
+
+]
 
 app.set("views", path.join(__dirname, 'views'))
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    res.render('user', user)
+    res.render('user', {users})
 })
 
 app.listen(3000, () => {
