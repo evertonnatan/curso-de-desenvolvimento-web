5b832f3c4dc8c1794cb718ca0b55bb803e33ac58
evertonnatan <evertonnatandl@outlook.com>
2021-03-21 08:58:54 -0300

    Substituiçao de variaveis com EJS

--- 24 - template_engine-EJS/templatecode/index.js
+++ 24 - template_engine-EJS/templatecode/index.js
@@ -3,11 +3,16 @@ const fs = require('fs')
 const path = require('path')
 const app = express()
 
+let user = {
+    name:"Everton Nathan", 
+    phone: "(566)7896-6541"
+}
+
 app.set("views", path.join(__dirname, 'views'))
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    res.render('user')
+    res.render('user', user)
 })
 
 app.listen(3000, () => {
